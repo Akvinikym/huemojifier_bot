b1655e8c5f7927546905d0bc2d553976088b7c20
Akvinikym <akvinikym@Pavels-MacBook-Pro.local>
2017-08-16 21:22:23 +0300

    foo

--- target/bin/worker.bat
+++ target/bin/worker.bat
@@ -76,7 +76,7 @@ goto endInit
 @REM Reaching here means variables are defined and arguments have been captured
 :endInit
 
-%JAVACMD% %JAVA_OPTS% %EXTRA_JVM_ARGUMENTS% -classpath %CLASSPATH_PREFIX%;%CLASSPATH% -Dapp.name="worker" -Dapp.repo="%REPO%" -Dbasedir="%BASEDIR%" WorkerProcess %CMD_LINE_ARGS%
+%JAVACMD% %JAVA_OPTS% %EXTRA_JVM_ARGUMENTS% -classpath %CLASSPATH_PREFIX%;%CLASSPATH% -Dapp.name="worker" -Dapp.repo="%REPO%" -Dbasedir="%BASEDIR%" Program %CMD_LINE_ARGS%
 if ERRORLEVEL 1 goto error
 goto end
 
